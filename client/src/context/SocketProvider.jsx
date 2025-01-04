e67e35d85a7ee07d5b9fe7235143ac7f5fa26d92
tripurarinathsingh <22353@iiitu.ac.in>
2025-01-04 17:12:25 +0530

    deploy

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(() => io("https://server-gjk5.onrender.com:8000"), []);
 
   return (
     <SocketContext.Provider value={socket}>
